d408c7a7570f5a762c5bff290147ff2145d68bad
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-03-06 20:25:43 +0530

    using kv language

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from kivy.app import App
 from kivy.uix.button import Button
 
-class MainApp(App):
+class ButtonApp(App):
     def build(self):
-        button = Button(text='Hello from Kivy',
-                        size_hint=(.5, .5),
-                        pos_hint={'center_x': .5, 'center_y': .5})
-        button.bind(on_press=self.on_press_button)
+        return Button()
 
-        return button
-
-    def on_press_button(self, instance):
+    def on_press_button(self):
         print('You pressed the button!')
 
 if __name__ == '__main__':
-    app = MainApp()
+    app = ButtonApp()
     app.run()
