45efa9dae64bed98132a4a8e7e30a724fdccaf27
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-03-06 20:13:29 +0530

    boxLayout

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import kivy
+import random
+
 from kivy.app import App
-from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
-class MainApp(App):
+red = [1,0,0,1]
+green = [0,1,0,1]
+blue =  [0,0,1,1]
+purple = [1,0,1,1]
+
+class HBoxLayoutExample(App):
     def build(self):
-        img = Image(source='logo.png',
-                    size_hint=(1, .5),
-                    pos_hint={'center_x':.5, 'center_y':.5})
-        return img
+        layout = BoxLayout(padding=10)
+        colors = [red, green, blue, purple]
 
-if __name__ == '__main__':
-    app = MainApp()
+        for i in range(5):
+            btn = Button(text="Button #%s" % (i+1),
+                         background_color=random.choice(colors)
+                         )
+
+            layout.add_widget(btn)
+        return layout
+
+if __name__ == "__main__":
+    app = HBoxLayoutExample()
     app.run()
