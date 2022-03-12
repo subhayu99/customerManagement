3e21e31c132d62f395d55431e94e0bf394254dea
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-03-12 14:50:20 +0530

    added widgets to select date and items

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
+#!/usr/bin/python3
+
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivymd.app import MDApp
+from datetime import datetime
 from kivy.lang import Builder
+from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from invoicepdf import invoice
+from kivymd.uix.picker import MDDatePicker, MDTimePicker
 
 Builder.load_file("main.kv")
 
 class MyGridLayout(Widget):
+    # Click OK
+    def on_booking_date_save(self, instance, value, date_range):
+        # print(instance, value, date_range)
+        self.ids.booking_date.text = str(value.strftime("%d/%m/%Y"))
+
+    def show_date_picker(self):
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_save=self.on_booking_date_save)
+        date_dialog.open()
+
+
+    def set_check_in_time(self, instance, time):
+        self.ids.check_in.text = str(time.strftime("%I:%M %p"))
+
+    def show_check_in_time_picker(self):
+        time_dialog = MDTimePicker()
+        time_dialog.set_time(datetime.strptime("12:00:00", '%H:%M:%S').time())
+        time_dialog.bind(time=self.set_check_in_time)
+        time_dialog.open()
+
+
+    def set_check_out_time(self, instance, time):
+        self.ids.check_out.text = str(time.strftime("%I:%M %p"))
+
+    def show_check_out_time_picker(self):
+        time_dialog = MDTimePicker()
+        time_dialog.set_time(datetime.strptime("10:00:00", '%H:%M:%S').time())
+        time_dialog.bind(time=self.set_check_out_time)
+        time_dialog.open()
+
+    def selected_item(self, value):
+        self.ids.item_desc.text += f"{value}\n"
+
     def press(self):
         invoice_num = self.ids.invoice_num.text
         customer = self.ids.customer.text
@@ -23,27 +61,52 @@ class MyGridLayout(Widget):
 
         items = []
         items_len = 0
-        for (i, j, k, l) in zip(item_desc, addons, price, qty):
-            item = [i, j, k, l]
-            items_len += len(''.join(item))
-            if items_len != 0:
-                items.append(item)
+        item_missing = False
+        if len(item_desc) == len(addons) == len(price) == len(qty):
+            if not "" in item_desc and not "" in addons and not "" in price and not "" in qty:
+                for (i, j, k, l) in zip(item_desc, addons, price, qty):
+                    item = [i, j, k, l]
+                    items_len += len(''.join(item))
+                    if items_len != 0:
+                        items.append(item)
+            else:
+                item_missing = True
+        else:
+            item_missing = True
 
 
         every_len = len(f"{invoice_num}{customer}{paid}{booking_date}{check_in}{check_out}{heads}")+items_len
-        print(every_len)
-        if every_len != 0:
-            invoice(invoice_num, customer, paid, booking_date, check_in, check_out, heads, items)
-            self.ids.message_label.text = "Details have been Submitted"
+        print(f"""
+        invoice_num: {invoice_num}
+        customer: {customer}
+        paid: {paid}
+        booking_date: {booking_date}
+        check_in: {check_in}
+        check_out: {check_out}
+        heads: {heads}
+        items: {items}
+        items_len: {items_len}
+        """)
+        if every_len:
+            if paid:
+                if not item_missing:
+                    print(items)
+                    invoice(invoice_num, customer, paid, booking_date, check_in, check_out, heads, items)
+                    self.ids.message_label.text = "Details have been Submitted"
+                else:
+                    self.ids.message_label.text = "Some items do not have values!"
+            else:
+                self.ids.message_label.text = "Amount Paid is missing!"
         else:
-            self.ids.message_label.text = "Some values are missing!!"
+            self.ids.message_label.text = "Some values are missing!"
 
-class SandCastles(App):
+class SandCastles(MDApp):
 
     def build(self):
         # change background color
-        Window.clearcolor = (1, 1, 1, 1)
-        
+        # Window.clearcolor = (1, 1, 1, 1)
+        self.theme_cls.theme_style = "Light"
+
         return MyGridLayout()
 
 
